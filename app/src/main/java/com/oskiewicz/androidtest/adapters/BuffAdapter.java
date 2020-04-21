c9be1e23b746f541e42688103be7217db51c876d
Sebastian <s.oskiewicz@gmail.com>
2020-04-21 13:57:29 +0200

    Add timer & clickable answer

--- app/src/main/java/com/oskiewicz/androidtest/adapters/BuffAdapter.java
+++ app/src/main/java/com/oskiewicz/androidtest/adapters/BuffAdapter.java
@@ -8,9 +8,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.mikhaellopez.circularimageview.CircularImageView;
 import com.oskiewicz.androidtest.R;
 import com.oskiewicz.androidtest.models.Answer;
 import com.squareup.picasso.Picasso;
@@ -22,12 +22,22 @@ public class BuffAdapter extends RecyclerView.Adapter<BuffAdapter.ViewHolder> {
     private Context context;
     private ArrayList<Answer> answerArrayList;
     private String answerImageUrl;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
 
 
     public BuffAdapter(Context context, ArrayList<Answer> answerArrayList) {
         this.context = context;
         this.answerArrayList = answerArrayList;
     }
+
 
     @NonNull
     @Override
@@ -51,11 +61,23 @@ public class BuffAdapter extends RecyclerView.Adapter<BuffAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         private AppCompatImageView answerImage;
         private TextView answerText;
+        private CardView cardView;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             answerText = itemView.findViewById(R.id.answerText);
             answerImage = itemView.findViewById(R.id.genericImage);
+            cardView = itemView.findViewById(R.id.answerCard);
+
+            itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
+                    }
+                }
+            });
         }
     }
 }
